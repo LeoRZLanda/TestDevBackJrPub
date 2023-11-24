a0eda389ec94945628123a7b8c7a329bce9e9208
LeoRZLanda <leorzlanda@gmail.com>
2023-11-23 18:52:38 -0600

    TestDevBackJr

---
+++ Prueba_2/Cons_2/Vistas/Index.cs
@@ -0,0 +1,12 @@
+namespace Cons_2.Vistas{
+    public class Index{
+
+
+        Console.WriteLine("Bienvenido a la solución para TestDevBackJr")
+
+        Console.WriteLine("\nSelecciona la opción de tu interes");
+
+        
+
+    }
+}
--- Prueba_2/Test_2/Interfaces/IRepositorioUsuarios.cs
+++
@@ -1,7 +0,0 @@
-using Test_2.Models;
-
-namespace Test_2.Interfaces{
-    public interface IRepositorioUsuarios {
-        public List<Usuario> Top10User();
-    }
-}
